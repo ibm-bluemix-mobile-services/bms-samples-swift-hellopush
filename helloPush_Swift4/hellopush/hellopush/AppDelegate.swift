8c260c63f50ef8ddf9251b3cdbabcde84f106774
Ananth <agirijak@in.ibm.com>
2017-12-11 13:58:18 +0530

    Swift 4 chnages

--- helloPush_Swift4/hellopush/hellopush/AppDelegate.swift
+++ helloPush_Swift4/hellopush/hellopush/AppDelegate.swift
@@ -7,6 +7,10 @@
 //
 
 import UIKit
+import BMSCore
+import BMSPush
+import UserNotifications
+import UserNotificationsUI
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,29 +20,138 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        UIApplication.shared.applicationIconBadgeNumber = 0;
         return true
     }
-
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    func registerForPush () {
+        
+        let myBMSClient = BMSClient.sharedInstance
+        myBMSClient.initialize(bluemixRegion: BMSClient.Region.usSouth)
+        let push =  BMSPushClient.sharedInstance
+        
+        push.initializeWithAppGUID(appGUID: "appGUID", clientSecret:"ClientSecret")
+        
+    }
+    func application (_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data){
+        
+        let push =  BMSPushClient.sharedInstance
+        push.registerWithDeviceToken(deviceToken: deviceToken) { (response, statusCode, error) -> Void in
+            
+            if error.isEmpty {
+                
+                print( "Response during device registration : \(String(describing: response))")
+                
+                print( "status code during device registration : \(String(describing: statusCode))")
+                let responseJson = self.convertStringToDictionary(text: response!)! as NSDictionary
+                let userId = responseJson.value(forKey: "userId")
+                
+                self.sendNotifToDisplayResponse(responseValue: "Device Registered Successfully with User ID \(String(describing: userId!))", responseBool: true)
+            }
+            else{
+                print( "Error during device registration \(error) ")
+                
+                self.sendNotifToDisplayResponse( responseValue: "Error during device registration \n  - status code: \(String(describing: statusCode)) \n Error :\(error) \n", responseBool: false)
+            }
+        }
+    }
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        
+        let message:String = "Error registering for push notifications: \(error.localizedDescription)"
+        
+        self.showAlert(title: "Registering for notifications", message: message)
+        
+        
+        
+    }
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        
+        let payLoad = ((((userInfo as NSDictionary).value(forKey: "aps") as! NSDictionary).value(forKey: "alert") as! NSDictionary).value(forKey: "body") as! String)
+        
+        self.showAlert(title: "Recieved Push notifications", message: payLoad)
+        
+    }
+    
+    func sendNotifToDisplayResponse (responseValue:String, responseBool: Bool){
+        
+        responseText = responseValue
+        isSuccess = responseBool
+        NotificationCenter.default.post(name: NSNotification.Name(rawValue: "action"), object: self);
     }
 
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    func showAlert (title:String , message:String){
+        
+        // create the alert
+        let alert = UIAlertController.init(title: title as String, message: message as String, preferredStyle: UIAlertControllerStyle.alert)
+        
+        // add an action (button)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+        
+        // show the alert
+        self.window!.rootViewController!.present(alert, animated: true, completion: nil)
     }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+    func convertStringToDictionary(text: String) -> [String:AnyObject]? {
+        if let data = text.data(using: String.Encoding.utf8) {
+            
+            guard let result = try? JSONSerialization.jsonObject(with: data, options: []) as? [String:AnyObject] else {
+                return [:]
+            }
+            return result
+        }
+        return [:]
     }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    
+    func unRegisterPush () {
+        
+        // MARK:  RETRIEVING AVAILABLE SUBSCRIPTIONS
+        
+        let push =  BMSPushClient.sharedInstance
+        
+        push.retrieveSubscriptionsWithCompletionHandler { (response, statusCode, error) -> Void in
+            
+            if error.isEmpty {
+                
+                print( "Response during retrieving subscribed tags : \(String(describing: response?.description))")
+                
+                print( "status code during retrieving subscribed tags : \(String(describing: statusCode))")
+                
+                // MARK:  UNSUBSCRIBING TO TAGS
+                
+                push.unsubscribeFromTags(tagsArray: response!, completionHandler: { (response, statusCode, error) -> Void in
+                    
+                    if error.isEmpty {
+                        
+                        print( "Response during unsubscribed tags : \(String(describing: response?.description))")
+                        
+                        print( "status code during unsubscribed tags : \(String(describing: statusCode))")
+                        
+                        // MARK:  UNSREGISTER DEVICE
+                        push.unregisterDevice(completionHandler: { (response, statusCode, error) -> Void in
+                            
+                            if error.isEmpty {
+                                
+                                print( "Response during unregistering device : \(String(describing: response))")
+                                
+                                print( "status code during unregistering device : \(String(describing: statusCode))")
+                                
+                                UIApplication.shared.unregisterForRemoteNotifications()
+                            }
+                            else{
+                                print( "Error during unregistering device \(error) ")
+                            }
+                        })
+                    }
+                    else {
+                        print( "Error during  unsubscribed tags \(error) ")
+                    }
+                })
+            }
+            else {
+                
+                print( "Error during retrieving subscribed tags \(error) ")
+            }
+            
+        }
+        
     }
 
 
